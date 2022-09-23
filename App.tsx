c522ffa075e71e5f9e41d7ab51c653f564c9f9dc
berg <berg.iodaniel@gmail.com>
2022-09-23 11:57:27 +0200

    prettier config

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, Provider as PaperProvider } from 'react-native-paper';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
     <PaperProvider>
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Button icon="camera">
-        Press me
-      </Button>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Button icon="camera">Press me</Button>
+        <StatusBar style="auto" />
+      </View>
     </PaperProvider>
   );
 }
@@ -20,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
