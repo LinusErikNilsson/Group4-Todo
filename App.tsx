d7804b2439dcce1186dacd23c96c0abe245013af
Linus Nilsson <linus.erik.nilsson@outlook.com>
2022-09-23 09:48:55 +0200

    init setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
+    <PaperProvider>
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button icon="camera">
+        Press me
+      </Button>
       <StatusBar style="auto" />
     </View>
+    </PaperProvider>
   );
 }
 
