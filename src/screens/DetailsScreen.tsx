70f96905c6d648a6f2c23ea4e2d7fce4a7206e4c
Ola Larsson <soaring.jr@hotmail.com>
2022-09-27 12:50:27 +0200

    Fixed context, added packages

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,33 +1,70 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Button, Text, ToggleButton } from "react-native-paper";
 import { RootStackParamList } from "../App";
+import { useTodo } from "../contexts/TodoContext";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Details">;
 
-export default function DetailsScreen({ navigation, route }: Props) {
-  return (
-    <View style={styles.container}>
-      <Text> Details Screen {route.params.id} 🔧</Text>
-      <StatusBar style="auto" />
-      <Button
-        title="Gå till detaljsidan igen"
-        onPress={() =>
-          navigation.push("Details", {
-            id: route.params.id + 1,
-            path: route.params.path + "/" + route.name,
-          })
-        }
-      />
-      <Button title="Gå tillbaka" onPress={() => navigation.goBack()} />
-      <Button title="Gå hem" onPress={() => navigation.navigate("Home")} />
-    </View>
-  );
+export default function DetailsScreen({ route }: Props) {
+  const { todoItems, updateTodo, removeTodo } = useTodo();
+  const todoData = todoItems.find((t) => t.id === route.params.id);
+  if (todoData) {
+    todoData.dueDate = new Date(todoData.dueDate);
+    return (
+      <View style={styles.container}>
+        <View style={styles.containerItem}>
+          <Text variant="headlineSmall">
+            {" "}
+            {todoData.title} {todoData.status}
+          </Text>
+          <ToggleButton
+            icon="check"
+            onPress={() => {
+              todoData.status =
+                todoData.status === "completed" ? "pending" : "completed";
+              updateTodo(todoData);
+            }}
+          />
+        </View>
+        <View style={styles.containerItem}>
+          <Text variant="headlineSmall"> Priority: {todoData.priority} </Text>
+          <Button
+            mode="contained"
+            icon="trash-can-outline"
+            onPress={() => {
+              removeTodo(todoData.id);
+            }}
+          >
+            Remove todo
+          </Button>
+        </View>
+        <View>
+          <Text variant="headlineSmall">
+            Due date: {todoData.dueDate.toDateString()}
+          </Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text variant="headlineSmall"> {todoData.description} </Text>
+        </View>
+        <View style={styles.containerItem}>
+          <Text variant="headlineSmall"> bleh </Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerItem: {
+    flex: 1,
+    flexDirection: "row",
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
